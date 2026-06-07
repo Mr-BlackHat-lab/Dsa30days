6a4334bd8214cc35b712d6fae2f171d4ded08b57
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2026-06-07 15:23:47 +0530

    merge sort done

--- Dsa30days/Dsa30days.cpp
+++ Dsa30days/Dsa30days.cpp
@@ -8,20 +8,25 @@
 using namespace std;
 int main()
 {
-    ArrayStack mystack(5);
+    ArrayStack mystack(9);
     if (mystack.isEmpty()) {
         cout << "stack is empty\n";
     }
     else {
         cout << "stack is not empty\n";
     }
-    mystack.push(20);
-    mystack.push(5);
-    mystack.push(40);
+    mystack.push(6);
+    mystack.push(4);
+    mystack.push(4);
     mystack.pop();
     cout << mystack.peek() << " is in top\n";
-    mystack.push(60);
-    mystack.push(50);
+    mystack.push(2);
+    mystack.push(1);
+    mystack.push(9);
+    mystack.push(8);
+    mystack.push(3);
+    mystack.push(5);
+    mystack.push(7);
     if (mystack.isEmpty()) {
         cout << "stack is empty\n";
     }
@@ -29,7 +34,7 @@ int main()
         cout << "stack is not empty\n";
     }
     mystack.trav();
-    mystack.Quicksort();
+    mystack.Mergesort();
     mystack.trav();
     mystack.pop();
 

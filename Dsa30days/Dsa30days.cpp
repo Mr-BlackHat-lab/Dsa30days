1ca7a504409f9b0756265bc0274dd0da22b3a293
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2026-06-11 14:09:18 +0530

    checked the code of counting sort and retargate to vs2026

--- Dsa30days/Dsa30days.cpp
+++ Dsa30days/Dsa30days.cpp
@@ -9,32 +9,40 @@ using namespace std;
 int main()
 {
     ArrayStack mystack(9);
-    if (mystack.isEmpty()) {
-        cout << "stack is empty\n";
-    }
-    else {
-        cout << "stack is not empty\n";
-    }
-    mystack.push(6);
-    mystack.push(4);
-    mystack.push(4);
-    mystack.pop();
-    cout << mystack.peek() << " is in top\n";
+	mystack.push(4);
+	mystack.push(2);
+    mystack.push(2);
     mystack.push(2);
     mystack.push(1);
-    mystack.push(9);
-    mystack.push(8);
     mystack.push(3);
+    mystack.push(0);
     mystack.push(5);
-    mystack.push(7);
-    if (mystack.isEmpty()) {
-        cout << "stack is empty\n";
-    }
-    else {
-        cout << "stack is not empty\n";
-    }
+    //if (mystack.isEmpty()) {
+    //    cout << "stack is empty\n";
+    //}
+    //else {
+    //    cout << "stack is not empty\n";
+    //}
+    //mystack.push(6);
+    //mystack.push(4);
+    //mystack.push(4);
+    //mystack.pop();
+    //cout << mystack.peek() << " is in top\n";
+    //mystack.push(2);
+    //mystack.push(1);
+    //mystack.push(9);
+    //mystack.push(8);
+    //mystack.push(3);
+    //mystack.push(5);
+    //mystack.push(7);
+    //if (mystack.isEmpty()) {
+    //    cout << "stack is empty\n";
+    //}
+    //else {
+    //    cout << "stack is not empty\n";
+    //}
     mystack.trav();
-    mystack.Mergesort();
+    mystack.CountingSort(0,5);
     mystack.trav();
     mystack.pop();
 

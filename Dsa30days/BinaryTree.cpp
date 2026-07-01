52a6b406f1c98a5704b02ac519c31886b760c522
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2026-07-01 20:15:06 +0530

    insert is done

--- Dsa30days/BinaryTree.cpp
+++ Dsa30days/BinaryTree.cpp
@@ -31,3 +31,28 @@ bool BinaryTree::search(int value) const {
 	}
 	return false;
 }
+Node* BinaryTree::insertHelper(Node* node, int value) {
+	if(node==nullptr){
+		Node* newNode = new Node;
+		newNode->data = value;
+		newNode->left = newNode->right = nullptr;
+		return newNode;
+	}
+	if (node->data < value) {
+		node->right = insertHelper(node->right, value);
+	}
+	else
+	{
+		node->left = insertHelper(node->left, value);
+	}
+	return node;
+}
+void BinaryTree::insert(int value) {
+	if (search(value)) {
+		std::cout << "Value:"<<value<<" already exits in tree";
+		return;
+	}
+	root = insertHelper(root, value);
+	std::cout << "value:" << value << " is sucssefully pushed";
+	node_count++;
+}

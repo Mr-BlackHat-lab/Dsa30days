476e2f8b8afe19b2393d5b3215d6e70ede474c80
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2026-07-01 19:42:22 +0530

    isEmpty and search is done

--- Dsa30days/BinaryTree.cpp
+++ Dsa30days/BinaryTree.cpp
@@ -14,3 +14,20 @@ void BinaryTree::destroy(Node* node) {
 	destroy(node->right);
 	delete node;
 }
+bool BinaryTree::isEmpty() const {
+	return root == nullptr;
+}
+bool BinaryTree::search(int value) const {
+	if (isEmpty()) return false;
+	Node* temp = root;
+	while (temp != nullptr) {
+		if (temp->data == value) return true;
+		if (temp->data < value) {
+			temp = temp->right;
+		}
+		else {
+			temp = temp->left;
+		}
+	}
+	return false;
+}

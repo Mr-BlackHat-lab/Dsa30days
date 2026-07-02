b03b8090671fabd88ccaef690681f734be1c0c39
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2026-07-02 18:00:40 +0530

    left and right rotation is implemented

--- Dsa30days/AvlTree.cpp
+++ Dsa30days/AvlTree.cpp
@@ -22,3 +22,34 @@ int AvlTree::getBalanceFactor(Node* node) {
 	if (!node) return 0;
 	return (getHeight(node->left) - getHeight(node->right));
 }
+Node* AvlTree::rightRotate(Node* node) {
+
+	Node* temp = node->left;
+	Node* temp2 = temp->right;
+
+
+	temp->right = node;
+	node->left = temp2;
+
+	node->height = std::max(getHeight(node->left), getHeight(node->right)) + 1;
+	temp->height = std::max(getHeight(temp->left), getHeight(temp->right)) + 1;
+
+	return temp;
+}
+
+Node* AvlTree::leftRotate(Node* node) {
+
+	Node* temp = node->right;
+	Node* temp2 = temp->left;
+
+
+	temp->left = node;
+	node->right = temp2;
+
+
+	node->height = std::max(getHeight(node->left), getHeight(node->right)) + 1;
+	temp->height = std::max(getHeight(temp->left), getHeight(temp->right)) + 1;
+
+
+	return temp;
+}

6275cb7aacd3b5e578c2cda0c11c17f66696673e
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2026-07-02 16:40:29 +0530

    avl tree start

---
+++ Dsa30days/AvlTree.h
@@ -0,0 +1,51 @@
+#pragma once
+
+struct Node
+{
+	int data;
+	int height;
+	Node* left;
+	Node* right;
+};
+
+class AvlTree
+{
+private:
+	Node* root;
+	int node_count;
+
+	int getHeight(Node* node);
+	int getBalanceFactor(Node* node);
+
+	Node* rightRotate(Node* y);
+	Node* leftRotate(Node* x);
+
+	//helpers
+	void destroy(Node* node);
+
+	Node* insertHelper(Node* node, int value);
+	Node* removeHelper(Node* node, int value);
+
+	int treeheightHelper(Node* node);
+	void preorderHelper(Node* node);
+	void postorderHelper(Node* node);
+	void inorderHelper(Node* node);
+
+public:
+	AvlTree();
+	~AvlTree();
+
+	bool isEmpty() const;
+
+	bool search(int value) const;
+	void insert(int value);
+	void remove(int value);
+
+	void peek_root() const;
+	void tree_height() const;
+
+	void pre_orderTraversal() const;
+	void post_orderTraversal() const;
+	void in_orderTraversal() const;
+
+};
